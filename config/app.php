852d70713ce0099bf62a1b67a0b404601aed2df6
javzero <Javzero1@gmail.com>
2019-05-10 18:40:36 -0300

    Modify app log level to single

--- config/app.php
+++ config/app.php
@@ -122,7 +122,7 @@ return [
     */
 
     // Laravel 5.6
-    // 'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'single'),
 
     // 'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
